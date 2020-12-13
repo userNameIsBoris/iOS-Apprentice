2f2dcc555bbe4d2be8bc5c1ee181378cdec165c7
Boris <BomEzhov@mail.ru>
2020-12-13 12:58:37 +0300

    Chapter 16 completed!

--- Bullseye (UIKit)/Bullseye/ViewController.swift
+++ Bullseye (UIKit)/Bullseye/ViewController.swift
@@ -8,22 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var sliderValue: Int = 50
+    @IBOutlet weak var slider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        slider.value = 50.0
         // Do any additional setup after loading the view.
     }
     
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hey", message: "This is not my first app", preferredStyle: .alert)
-        
+        let message = "The value of the slider is: \(sliderValue)"
+
+        let alert = UIAlertController(title: "Hey", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
         alert.addAction(action)
-        
+
         present(alert, animated: true, completion: nil)
-        
-        
     }
 
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        sliderValue = lroundf(sender.value)
+    }
 }
 
