bbbd1fe631d155d769057dc01d7a946ad5524df1
Boris <BomEzhov@mail.ru>
2020-12-13 11:55:30 +0300

    Chapter 15 completed!

---
+++ Bullseye (UIKit)/Bullseye/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Bullseye
+//
+//  Created by Борис on 13.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hey", message: "This is not my first app", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+        
+    }
+
+}
+
