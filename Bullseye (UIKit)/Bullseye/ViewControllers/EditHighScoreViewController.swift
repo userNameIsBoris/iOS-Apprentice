358a898470926cd24f72d266c8d97d9d795fa470
Boris <BomEzhov@mail.ru>
2020-12-22 20:11:35 +0300

    Chapter 23 completed!

--- Bullseye (UIKit)/Bullseye/ViewControllers/EditHighScoreViewController.swift
+++ Bullseye (UIKit)/Bullseye/ViewControllers/EditHighScoreViewController.swift
@@ -7,10 +7,17 @@
 
 import UIKit
 
-class EditHighScoreViewController: UITableViewController {
+class EditHighScoreViewController: UITableViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        textField.becomeFirstResponder()
+    }
 
     // MARK: - Actions
-
     @IBAction func done() {
         navigationController?.popViewController(animated: true)
     }
@@ -21,5 +28,21 @@ class EditHighScoreViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    // MARK: - Table View Delegates
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+
+    // MARK: - Text Field Delegates
+    func textField(_ UITextField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+
+        doneBarButton.isEnabled = !newText.isEmpty
+        return true
     }
 }
--- Bullseye (UIKit)/Bullseye/ViewControllers/HighScoresViewController.swift
+++ Bullseye (UIKit)/Bullseye/ViewControllers/HighScoresViewController.swift
@@ -58,7 +58,7 @@ class HighScoresViewController: UITableViewController {
 
         PersistencyHelper.saveHighScores(items)
     }
-    
+
     // MARK: - Actions
 
     @IBAction func resetHighScores() {
@@ -93,38 +93,4 @@ class HighScoresViewController: UITableViewController {
         tableView.reloadData()
         PersistencyHelper.saveHighScores(items)
     }
-    
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
