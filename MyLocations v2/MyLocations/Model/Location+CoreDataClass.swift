e756a301059ab0896db90f0031a568389f5b4d24
Boris <BomEzhov@mail.ru>
2021-02-15 16:09:32 +0300

    Complete chapter 29

--- MyLocations v2/MyLocations/Model/Location+CoreDataClass.swift
+++ MyLocations v2/MyLocations/Model/Location+CoreDataClass.swift
@@ -8,8 +8,23 @@
 
 import Foundation
 import CoreData
+import MapKit
 
 @objc(Location)
 public class Location: NSManagedObject {
 
+}
+
+extension Location: MKAnnotation {
+  public var coordinate: CLLocationCoordinate2D {
+    return CLLocationCoordinate2DMake(latitude, longitude)
+  }
+
+  public var title: String? {
+    return locationDescription.isEmpty ? "(No description)" : locationDescription
+  }
+
+  public var subtitle: String? {
+    return category
+  }  
 }
--- MyLocations v2/MyLocations/SceneDelegate.swift
+++ MyLocations v2/MyLocations/SceneDelegate.swift
@@ -28,6 +28,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
       navController = tabViewControllers[1] as! UINavigationController
       let controller2 = navController.viewControllers.first as! LocationsViewController
       controller2.managedObjectContext = managedObjectContext
+
+      // Third tab
+      navController = tabViewControllers[2] as! UINavigationController
+      let controller3 = navController.viewControllers.first as! MapViewController
+      controller3.managedObjectContext = managedObjectContext
     }
     listenForFatalCoreDataNotifications()
   }
